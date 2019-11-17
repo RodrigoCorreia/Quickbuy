820ce28dc8b2d48f54bf663ca5c996d158443c9f
Rodrigo Correia <rodrigocorrei@gmail.com>
2019-11-17 17:40:30 -0300

    Add entitys to the project.

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuickBuy.Dominio.Entidades
+{
+	public class Pedido
+	{
+		public int Id { get; set; }
+		public DateTime DataPedido { get; set; }
+		public int UsuarioId { get; set; }
+		/// <summary>
+		/// Pedido deve ter pelo menos um pedido
+		/// ou pelo menos muitos pedidos
+		/// </summary>
+		public ICollection<ItemPedido> ItensPedidos { get; set; }
+	}
+}
