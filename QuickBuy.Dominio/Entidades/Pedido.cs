312de91884b19878723dcf1e103da47121c6bab9
Rodrigo Correia <rodrigocorrei@gmail.com>
2019-11-17 17:54:35 -0300

    Add new changes.

--- QuickBuy.Dominio/Entidades/Pedido.cs
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
 using System.Collections.Generic;
 
 namespace QuickBuy.Dominio.Entidades
@@ -8,9 +9,19 @@ namespace QuickBuy.Dominio.Entidades
 		public int Id { get; set; }
 		public DateTime DataPedido { get; set; }
 		public int UsuarioId { get; set; }
+
+		public DateTime DataPrevisaoEntrega { get; set; }
+		public string Cep { get; set; }
+		public string Estado { get; set; }
+		public string Cidade { get; set; }
+		public string EnderecoCompleto { get; set; }
+		public int NumeroEndereco { get; set; }
+		public int FormaPagamentoId { get; set; }
+		public FormaPagamento FormaPagamento { get; set; }
+
 		/// <summary>
-		/// Pedido deve ter pelo menos um pedido
-		/// ou pelo menos muitos pedidos
+		/// Pedido deve ter pelo menos um item de pedido
+		/// ou pelo menos muitos itens de pedidos
 		/// </summary>
 		public ICollection<ItemPedido> ItensPedidos { get; set; }
 	}
